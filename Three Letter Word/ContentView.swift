46619c79c8ba898079eb8fbdf0203d7881a49948
Peter Hutchison <Peterhutch03@gmail.com>
2021-10-27 10:54:28 -0500

    Finished Product

--- Three Letter Word/ContentView.swift
+++ Three Letter Word/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var letter = ""
     @State private var counter = 0
+    @State private var selectedLetters = ["", "", ""]
     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var body: some View {
         VStack {
@@ -18,9 +19,28 @@ struct ContentView: View {
                 .padding()
                 .font(.title)
             Text("Tap the grey box to change the letter")
+            CustomLetterBox(color: Color.gray, text: letter)
+                .onTapGesture {
+                    let position = alphabet.index(alphabet.startIndex, offsetBy: counter)
+                    letter = String(alphabet[position])
+                    counter += 1
+                    if counter == alphabet.count {
+                        counter = 0
+                    }
+                }
+            Text("Tap a red box to select that letter")
+            HStack {
+                ForEach(0..<3) { index in
+                    CustomLetterBox(color: .red, text: selectedLetters[index])
+                    .onTapGesture {
+                        selectedLetters[index] = letter
+                    }
+                }
+            }
         }
     }
 }
+
 struct CustomLetterBox : View {
     let color : Color
     let text : String
@@ -30,8 +50,11 @@ struct CustomLetterBox : View {
             Text(text)
                 .font(.system(size: 90))
                 .fontWeight(.heavy)
+        }
+        .frame(width: 120, height: 120, alignment: .center)
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
